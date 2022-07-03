efc7821a20dcdf4516f5d88a1b43ab9a8b0e0637
islandskan <sigridureggertsdottir.art@gmail.com>
2022-07-03 13:21:45 +0200

    extend color classes and font classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: 'jit',
-    content: ['./*.html', './build/index.html'],
+    content: ['./build/index.html'],
     theme: {
         extend: {
             colors: {
                 primary: '#F2994A',
+                secondary: {
+                    100: '#4F4F4F',
+                    200: '#BDBDBD',
+                    300: '#333333',
+                    400: '#4F4F4F',
+                    500: '#828282',
+                },
             },
             fontFamily: {
                 heading: ['"Playfair Display"'],
